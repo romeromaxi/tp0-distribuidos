053cf680e3c7bbde0ee63044ef1e3104ff815585
romeromaxi <mromerov@fi.uba.ar>
2024-08-31 22:30:04 -0300

    feat: add connectionHandler base - Client

---
+++ client/common/connectionHandler.go
@@ -0,0 +1,56 @@
+package common
+
+import (
+	"net"
+)
+
+type ConnectionHandler struct {
+	conn net.Conn
+}
+
+func NewConnectionHandler(conn net.Conn) *ConnectionHandler {
+	ConnectionHandler := &ConnectionHandler{
+		conn: conn,
+	}
+	return ConnectionHandler
+}
+
+func (mc *ConnectionHandler) send(data []byte) error {
+	sizeToSend := len(data)
+
+	totalBytesSent := 0
+	for totalBytesSent < sizeToSend {
+		bytesSent, err := mc.conn.Write(data[totalBytesSent:])
+
+		if err != nil {
+			return err
+		}
+
+		totalBytesSent += bytesSent
+	}
+
+	return nil
+}
+
+func (mc *ConnectionHandler) recv(sizeToRecv int) ([]byte, error) {
+	data := make([]byte, sizeToRecv)
+
+	totalBytesRecv := 0
+	for totalBytesRecv < sizeToRecv {
+		bytesRecv, err := mc.conn.Read(data[totalBytesRecv:])
+
+		if err != nil {
+			return nil, err
+		}
+
+		totalBytesRecv += bytesRecv
+	}
+
+	return data, nil
+}
+
+func (c *ConnectionHandler) close() {
+	if c.conn != nil {
+		c.conn.Close()
+	}
+}
